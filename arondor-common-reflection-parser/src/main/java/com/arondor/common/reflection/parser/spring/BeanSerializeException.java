2275a4d45a8593392c9a805d38d02c4c625bc2be
Caroline Gaudin <caroline.gaudin@arondor.com>
2014-03-12 15:31:59 +0100

    Add exception used for bean serialize exception. Replace RuntimeException with BeanSerializeException.

---
+++ arondor-common-reflection-parser/src/main/java/com/arondor/common/reflection/parser/spring/BeanSerializeException.java
@@ -0,0 +1,15 @@
+package com.arondor.common.reflection.parser.spring;
+
+public class BeanSerializeException extends RuntimeException
+{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 8855146912447149417L;
+
+    public BeanSerializeException(String message)
+    {
+        super(message);
+    }
+}
