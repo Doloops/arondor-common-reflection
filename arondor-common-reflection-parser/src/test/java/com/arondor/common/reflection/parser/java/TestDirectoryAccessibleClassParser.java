578e4b4725e229d2cc3c317d0ac4bc2eb1f39ccc
Caroline <Caroline@192.168.0.108>
2014-03-20 19:44:10 +0100

    Add test to load lib from file system

---
+++ arondor-common-reflection-parser/src/test/java/com/arondor/common/reflection/parser/java/TestDirectoryAccessibleClassParser.java
@@ -0,0 +1,41 @@
+package com.arondor.common.reflection.parser.java;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.arondor.common.reflection.api.catalog.AccessibleClassCatalog;
+import com.arondor.common.reflection.catalog.SimpleAccessibleClassCatalog;
+
+public class TestDirectoryAccessibleClassParser
+{
+    @Test
+    public void testParseLibsFolder() throws ClassNotFoundException
+    {
+        AccessibleClassCatalog catalog = new SimpleAccessibleClassCatalog();
+        DirectoryAccessibleClassProvider provider = new DirectoryAccessibleClassProvider();
+        JavaAccessibleClassParser parser = new JavaAccessibleClassParser();
+        parser.setTryInstantiateClassForDefaultValue(true);
+        provider.setAccessibleClassParser(parser);
+
+        List<String> packagePrefixes = Arrays.asList("org.junit");
+        provider.setPackagePrefixes(packagePrefixes);
+        List<String> directories = Arrays.asList("src/test/resources/libs");
+        provider.setDirectories(directories);
+        provider.provideClasses(catalog);
+
+        Assert.assertNotNull(catalog.getAccessibleClass("org.junit.Test"));
+        try
+        {
+            catalog.getAccessibleClass("org.apache.log4j.Logger");
+            Assert.fail();
+        }
+        catch (ClassNotFoundException e)
+        {
+
+        }
+
+    }
+}
