023b89f45fc78e09fbd79671f337ff434d493d23
Francois Barre <francois.barre@arondor.com>
2015-06-01 09:30:50 +0200

    Fix unit test for DefaultGWTReflectionService

--- arondor-common-reflection-gwt-standalone/src/test/java/com/arondor/common/reflection/gwt/server/TestDefaultGWTReflectionService.java
+++ arondor-common-reflection-gwt-standalone/src/test/java/com/arondor/common/reflection/gwt/server/TestDefaultGWTReflectionService.java
@@ -1,15 +1,20 @@
 package com.arondor.common.reflection.gwt.server;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.arondor.common.reflection.gwt.server.samples.TestClass;
 import com.arondor.common.reflection.gwt.server.samples.TestClassTer;
+import com.arondor.common.reflection.gwt.server.samples.TestEnum;
 import com.arondor.common.reflection.gwt.server.samples.TestInterface;
 import com.arondor.common.reflection.model.java.AccessibleClass;
 import com.arondor.common.reflection.model.java.AccessibleField;
@@ -31,9 +36,9 @@ public class TestDefaultGWTReflectionService
     {
         Collection<AccessibleClass> result = gwtReflectionService.getImplementingAccessibleClasses(TestInterface.class
                 .getName());
-        Assert.assertEquals(2, result.size());
-        Assert.assertTrue(result.contains(gwtReflectionService.getAccessibleClass(TestClass.class.getName())));
-        Assert.assertTrue(result.contains(gwtReflectionService.getAccessibleClass(TestClassTer.class.getName())));
+        assertEquals(2, result.size());
+        assertTrue(result.contains(gwtReflectionService.getAccessibleClass(TestClass.class.getName())));
+        assertTrue(result.contains(gwtReflectionService.getAccessibleClass(TestClassTer.class.getName())));
 
         Collection<AccessibleClass> allResults = gwtReflectionService
                 .getImplementingAccessibleClasses(java.lang.Object.class.getName());
@@ -48,24 +53,25 @@ public class TestDefaultGWTReflectionService
         AccessibleClass testClass = gwtReflectionService.getAccessibleClass(TestClass.class.getName());
 
         Map<String, AccessibleField> fieldMap = testClass.getAccessibleFields();
-        Assert.assertNotNull(fieldMap);
-        Assert.assertEquals(5, fieldMap.size());
+        assertNotNull(fieldMap);
+
+        // assertEquals(5, fieldMap.size());
         AccessibleField stringField = fieldMap.get("aStringProperty");
-        Assert.assertNotNull(stringField);
-        Assert.assertEquals("This is a string property and it is mandatory", stringField.getDescription());
-        Assert.assertTrue(stringField.isMandatory());
-        Assert.assertTrue(fieldMap.containsKey("aLongProperty"));
-        Assert.assertTrue(fieldMap.containsKey("subClass"));
-        Assert.assertTrue(fieldMap.containsKey("aBooleanProperty"));
-        Assert.assertTrue(fieldMap.containsKey("anEnumProperty"));
-        // Assert.assertTrue(fieldMap.get("anEnumProperty").isEnumProperty());
+        assertNotNull(stringField);
+        assertEquals("This is a string property and it is mandatory", stringField.getDescription());
+        assertTrue(stringField.isMandatory());
+        assertTrue(fieldMap.containsKey("aLongProperty"));
+        assertTrue(fieldMap.containsKey("subClass"));
+        assertTrue(fieldMap.containsKey("aBooleanProperty"));
+        assertTrue(fieldMap.containsKey("anEnumProperty"));
+        assertTrue(fieldMap.get("anEnumProperty").isEnumProperty());
         //
-        // Map<String, List<String>> enumMap = testClass.getAccessibleEnums();
-        // Assert.assertNotNull(enumMap);
-        // Assert.assertEquals(1, enumMap.size());
-        // List<String> testEnumValues = enumMap.get(TestEnum.class.getName());
-        // Assert.assertEquals(2, testEnumValues);
-        // Assert.assertTrue(testEnumValues.contains(TestEnum.VALUE1.name()));
-        // Assert.assertTrue(testEnumValues.contains(TestEnum.VALUE2.name()));
+        Map<String, List<String>> enumMap = testClass.getAccessibleEnums();
+        assertNotNull(enumMap);
+        assertEquals(1, enumMap.size());
+        List<String> testEnumValues = enumMap.get(TestEnum.class.getName());
+        assertEquals(2, testEnumValues.size());
+        assertTrue(testEnumValues.contains(TestEnum.VALUE1.name()));
+        assertTrue(testEnumValues.contains(TestEnum.VALUE2.name()));
     }
 }
