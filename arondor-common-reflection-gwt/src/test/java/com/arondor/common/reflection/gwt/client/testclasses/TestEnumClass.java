9817d10e672590b2249fa92a5b1a4051c9b88f1e
Joseph Tessier <joseph.tessier@arondor.com>
2019-11-20 10:55:06 +0100

    add test cases for enum type

---
+++ arondor-common-reflection-gwt/src/test/java/com/arondor/common/reflection/gwt/client/testclasses/TestEnumClass.java
@@ -0,0 +1,17 @@
+package com.arondor.common.reflection.gwt.client.testclasses;
+
+public class TestEnumClass
+{
+    private TestEnumDay today;
+
+    public TestEnumDay getToday()
+    {
+        return today;
+    }
+
+    public void setToday(TestEnumDay today)
+    {
+        this.today = today;
+    }
+
+}
---
+++ arondor-common-reflection-gwt/src/test/java/com/arondor/common/reflection/gwt/client/testclasses/TestEnumDay.java
@@ -0,0 +1,6 @@
+package com.arondor.common.reflection.gwt.client.testclasses;
+
+public enum TestEnumDay
+{
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
