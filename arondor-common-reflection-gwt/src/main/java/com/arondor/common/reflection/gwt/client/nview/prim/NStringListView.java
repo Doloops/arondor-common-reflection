7ec86245d5da77ef61e075d8406bae27f7d9621e
Joe Tessier <joseph.tessier@arondor.com>
2019-12-13 17:38:05 +0100

    #3 #4 #7 - StringList input
    - resetFieldBtn placing and styling, handlers (#3)
    - code cleaning (#7)
    - placeholder/default values consideration (#4)

--- arondor-common-reflection-gwt/src/main/java/com/arondor/common/reflection/gwt/client/nview/prim/NStringListView.java
+++ arondor-common-reflection-gwt/src/main/java/com/arondor/common/reflection/gwt/client/nview/prim/NStringListView.java
@@ -9,34 +9,62 @@ import com.arondor.common.reflection.gwt.client.presenter.fields.StringListTreeN
 import com.arondor.common.reflection.gwt.client.view.MyValueChangeEvent;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
 import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.TextArea;
 
 public class NStringListView extends NNodeView implements StringListDisplay
 {
     protected final TextArea textArea = new TextArea();
 
-    private String defaultValue = "";
+    private FlowPanel inputGroupPanel = new FlowPanel();
+
+    private List<String> defaultValue = new ArrayList<String>();
 
     public NStringListView()
     {
         getElement().addClassName(CssBundle.INSTANCE.css().stringField());
 
-        addChildren();
+        getResetFieldBtn().getElement().addClassName("input-group-append");
+        getResetFieldBtn().getElement().addClassName(CssBundle.INSTANCE.css().resetFieldBtn());
+        getResetFieldBtn().getElement().setInnerHTML("<span class=\"input-group-text\"><i></i></span>");
+
+        getResetFieldBtn().addClickHandler(new ClickHandler()
+        {
+
+            @Override
+            public void onClick(ClickEvent event)
+            {
+                setDefaultValue(defaultValue);
+            }
+        });
+
+        textArea.getElement().setAttribute("placeholder", "value A\nvalue B");
+
+        textArea.getElement().addClassName("form-control");
+        inputGroupPanel.getElement().addClassName("input-group");
+
+        // setDefaultValue(Arrays.asList("a", "b", "c"));
 
         bind();
-    }
 
-    private void addChildren()
-    {
-        add(textArea);
+        attachHandlers();
     }
 
     private void bind()
+    {
+        inputGroupPanel.add(textArea);
+        inputGroupPanel.add(getResetFieldBtn());
+        add(inputGroupPanel);
+    }
+
+    private void attachHandlers()
     {
         textArea.addKeyUpHandler(new KeyUpHandler()
         {
@@ -61,25 +89,20 @@ public class NStringListView extends NNodeView implements StringListDisplay
     private void checkActive()
     {
         String input = textArea.getValue();
-        setActive(!input.equals(defaultValue));
+        setActive(!input.equals(defaultValue.toString()));
     }
 
     @Override
     public void setValue(List<String> values)
     {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String value : values)
-        {
-            stringBuilder.append(value);
-            stringBuilder.append('\n');
-        }
-        textArea.setValue(stringBuilder.toString());
+        textArea.setValue(String.join("\n", values));
         setActive(true);
     }
 
     @Override
     public void setDefaultValue(List<String> value)
     {
+        this.defaultValue = value;
         setValue(value);
         setActive(false);
     }
@@ -106,13 +129,13 @@ public class NStringListView extends NNodeView implements StringListDisplay
     @Override
     public void setPlaceholder(List<String> value)
     {
-        textArea.getElement().setAttribute("placeholder", value.get(0));
+        textArea.getElement().setAttribute("placeholder", String.join("\n", value));
     }
 
     @Override
     public void clear()
     {
         super.clear();
-        addChildren();
+        bind();
     }
 }
