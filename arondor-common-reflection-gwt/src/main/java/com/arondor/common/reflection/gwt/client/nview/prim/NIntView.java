32be2ea90f62b838d0e1c0315166b20545cbb005
Joseph Tessier <joseph.tessier@arondor.com>
2020-01-02 10:09:51 +0100

    Prevent negative values on scroll in integer input (e.g. nb docs/punnet)

--- arondor-common-reflection-gwt/src/main/java/com/arondor/common/reflection/gwt/client/nview/prim/NIntView.java
+++ arondor-common-reflection-gwt/src/main/java/com/arondor/common/reflection/gwt/client/nview/prim/NIntView.java
@@ -1,9 +1,12 @@
 package com.arondor.common.reflection.gwt.client.nview.prim;
 
+import gwt.material.design.client.constants.InputType;
+
 public class NIntView extends NStringView
 {
     public NIntView()
     {
-        getTextBox().getElement().getElementsByTagName("input").getItem(0).setAttribute("type", "number");
+        getTextBox().setType(InputType.NUMBER);
+        getTextBox().getElement().getElementsByTagName("input").getItem(0).setAttribute("min", "0");
     }
 }
